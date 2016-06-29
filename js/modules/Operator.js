0e5841db07a1000326257713160e8d3237e9b85a
luw <luw@mysoft.com.cn>
2016-06-29 16:20:52 +0800

    add three.js

---
+++ js/modules/Operator.js
@@ -0,0 +1,55 @@
+/**
+ * Created by luw on 2016/6/29.
+ */
+
+function Operator() {};
+
+Operator.prototype.onWindowResize = function() {
+    var windowHalfX = window.innerWidth / 2;
+    var windowHalfY = window.innerHeight / 2;
+
+    builder.camera.aspect = window.innerWidth / window.innerHeight;
+    builder.camera.updateProjectionMatrix();
+    builder.renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+Operator.prototype.onDocumentMouseDown = function( event ) {
+    event.preventDefault();
+    builder.isUserInteracting = true;
+    builder.onPointerDownPointerX = event.clientX;
+    builder.onPointerDownPointerY = event.clientY;
+    builder.onPointerDownLon = builder.lon;
+    builder.onPointerDownLat = builder.lat;
+}
+
+Operator.prototype.onDocumentMouseMove = function( event ) {
+    if ( builder.isUserInteracting === true ) {
+        builder.lon = ( builder.onPointerDownPointerX - event.clientX ) * 0.1 + builder.onPointerDownLon;
+        builder.lat = ( event.clientY - builder.onPointerDownPointerY ) * 0.1 + builder.onPointerDownLat;
+    }
+}
+
+Operator.prototype.onDocumentMouseUp = function( event ) {
+    builder.isUserInteracting = false;
+}
+
+Operator.prototype.onDocumentMouseWheel = function( event ) {
+    builder.camera.fov -= event.wheelDeltaY * 0.05;
+    builder.camera.updateProjectionMatrix();
+}
+
+Operator.prototype.render = function() {
+    if ( builder.isUserInteracting === false ) {
+        builder.lon += 0.1;
+    }
+    console.log(builder.lat);
+    builder.lat = Math.max( - 85, Math.min( 85, builder.lat ) );
+    builder.phi = THREE.Math.degToRad( 90 - builder.lat );
+    builder.theta = THREE.Math.degToRad( builder.lon );
+    builder.camera.position.x = -500 * Math.sin( builder.phi ) * Math.cos( builder.theta );
+    builder.camera.position.y = 500 * Math.cos( builder.phi );
+    builder.camera.position.z = 500 * Math.sin( builder.phi ) * Math.sin( builder.theta );
+
+    builder.camera.lookAt( builder.scene.position );
+    builder.renderer.render( builder.scene, builder.camera );
+}
