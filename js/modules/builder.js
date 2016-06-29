16b6a7f8def6ee1b947d04ff1e70791850c80eca
luw <luw@mysoft.com.cn>
2016-06-29 16:30:26 +0800

    add three.js

--- js/modules/builder.js
+++ js/modules/builder.js
@@ -66,6 +66,28 @@ Builder.prototype.initSettingbar = function() {
 }
 
 Builder.prototype.initBackground = function() {
+    var size = 500, step = 50;
+    var geometry = new THREE.Geometry();
+    for ( var i = - size; i <= size; i += step ) {
+        geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
+        geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+        geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
+        geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+    }
+    var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } );
+    var line = new THREE.LineSegments( geometry, material );
+    this.scene.add( line );
+}
+
+Builder.prototype.initRenderer = function() {
+    this.renderer = new THREE.CanvasRenderer();
+    this.renderer.setClearColor( 0xf0f0f0 );
+    this.renderer.setPixelRatio( window.devicePixelRatio );
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.container.appendChild( this.renderer.domElement );
+}
+
+Builder.prototype.initObjects = function() {
     var geometry = new THREE.BoxGeometry( 50, 100, 50 );
     for ( var i = 0; i < geometry.faces.length; i += 2 ) {
         var hex = Math.random() * 0xffffff;
@@ -80,29 +102,5 @@ Builder.prototype.initBackground = function() {
     cube.position.y = 0;
     cube.position.z = 0;
     this.scene.add(cube);
-}
-
-Builder.prototype.initRenderer = function() {
-    this.renderer = new THREE.CanvasRenderer();
-    this.renderer.setClearColor( 0xf0f0f0 );
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
-    this.container.appendChild( this.renderer.domElement );
-}
-
-Builder.prototype.initObjects = function() {
-    var geometry = new THREE.BoxGeometry( 100, 100, 100 );
-    for ( var i = 0; i < geometry.faces.length; i += 2 ) {
-        var hex = Math.random() * 0xffffff;
-        //var hex = 0x1133ff;
-        geometry.faces[ i ].color.setHex( hex );
-        geometry.faces[ i + 1 ].color.setHex( hex );
-    }
-    var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
-    cube = new THREE.Mesh( geometry, material );
-    cube.position.x = 0;
-    cube.position.y = 0;
-    cube.position.z = 0;
-    this.scene.add( cube );
     this.objects.push( cube );
 }
