79400c963405b84b64f8e16bf30ed91ebd17f8ab
luw <luw@mysoft.com.cn>
2016-06-29 11:34:45 +0800

    add three.js

---
+++ js/modules/builder.js
@@ -0,0 +1,13 @@
+/**
+ * Created by luw on 2016/6/29.
+ */
+function Builder(scene) {
+    this.scene = scene;
+}
+
+Builder.prototype.background = function() {
+    alert(this.scene);
+}
+
+var builder = new Builder('11');
+builder.background();
